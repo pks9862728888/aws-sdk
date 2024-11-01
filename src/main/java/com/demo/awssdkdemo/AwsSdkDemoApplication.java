43a1789d9668358d6e8dbf6d223adc2b2a4c0fa2
pks9862728888 <pks9862728888@gmail.com>
2024-11-01 10:25:46 +0530

    Initial commit

---
+++ src/main/java/com/demo/awssdkdemo/AwsSdkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.awssdkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSdkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSdkDemoApplication.class, args);
+	}
+
+}
